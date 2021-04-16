1271c870575a4b4655abe95bed5597660c87c8b5
mehrshad <mehrshad.karimi@yekpay.com>
2021-04-16 12:25:22 +0430

    add project files

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Contact.styles.scss';
+
+function Contact(props) {
+    return (
+        <div className="col-12 contact-page">
+
+                <div className=" contact-container">
+                    <h2>
+                        Designed And Developed By:
+                    </h2>
+                    <h1>
+                        Fatemeh Ramzanghorbani
+                    </h1>
+                    <h3>
+                        Email:Fatemeh.ramzanghorbani7@gmail.com
+                    </h3>
+                    <h3>
+                        Student number: 982011792
+                    </h3>
+                </div>
+        </div>
+    );
+}
+
+export default Contact;
